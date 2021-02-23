a454068d0bc7b2c5398f171e572a5912c6d3f16e
SammyStonique <sammystonique@gmail.com>
2021-02-23 12:53:43 +0300

    added django-heroku,updated requirements.txt

--- EventMgtSystem/settings.py
+++ EventMgtSystem/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +140,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ezenfinancialsevents@gmail.com'
 EMAIL_HOST_PASSWORD = 'eventsmanagement'
+
+django_heroku.settings(locals())
