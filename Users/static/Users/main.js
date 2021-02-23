9c05e5e49e300bb0e5e4bd619ecf70d26d5349a8
SammyStonique <sammystonique@gmail.com>
2021-02-23 17:08:43 +0300

    updated base2.html with jquery

--- Users/static/Users/main.js
+++ Users/static/Users/main.js
@@ -1,6 +1,11 @@
 function reveal() {
     if (document.getElementById('box').checked) {
         document.getElementById("id_password").type = 'text';
-    } else
+        document.getElementById("id_new_password1").type = 'text';
+        document.getElementById("id_new_password2").type = 'text';
+    } else {
         document.getElementById("id_password").type = 'password';
+        document.getElementById("id_new_password1").type = 'password';
+        document.getElementById("id_new_password2").type = 'password';
+    }
 }
