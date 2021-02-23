bae2621dc9c301275385e8e4a11a7c1e21e23fba
SammyStonique <sammystonique@gmail.com>
2021-02-23 15:19:02 +0300

    updated users/main.js

--- Users/static/Users/main.js
+++ Users/static/Users/main.js
@@ -1,3 +1,11 @@
+/* Null check on updateHTML */
+function updateHTML(elmId, value) {
+    var elem = document.getElementById(elmId);
+    if (typeof elem !== 'undefined' && elem !== null) {
+        elem.innerHTML = value;
+    }
+}
+
 function reveal() {
     if (document.getElementById('box').checked) {
         document.getElementById("pw").type = 'text';
